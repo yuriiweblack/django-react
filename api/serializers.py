294fce2e52eaa4ad42bdde19af1e10e0cd721770
yuriiweblack <yuriiwebblack@gmail.com>
2022-01-25 12:04:21 +0200

    rest_framework_backend

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'quest_can_pause',
+        'votes_to_skip','created_at')
+
